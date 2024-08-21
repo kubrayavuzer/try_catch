f0086457fb5d99cfef8e2593ec7575be4684d58e
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-21 03:30:04 +0300

    Proje dosyası ekle.

---
+++ try_catch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Bir sayı girin: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int kare = sayi * sayi;
+            Console.WriteLine("Girilen sayının karesi: " + kare);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş yaptınız! ;Bir sayı giriniz.");
+        }
+    }
+}
